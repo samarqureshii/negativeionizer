99f962b1bba01f5aec28c11e3dfc48308e0f1fda
samarqureshii <63826930+samarqureshii@users.noreply.github.com>
2021-03-19 21:20:03 -0400

    Add files via upload
    fully working version with MQ135, motor, and distance sensor

---
+++ sketch_mar19a.ino
@@ -0,0 +1,59 @@
+int inPin = 2;
+int trigPin = 5;
+int echoPin = 6;
+int leftMotor_forward = 9;
+int leftMotor_backward = 10;
+int rightMotor_forward = 11;
+int rightMotor_backward = 12;
+long duration;
+int distance, sensorValue;
+
+void setup() {
+pinMode(leftMotor_forward, OUTPUT);
+pinMode(leftMotor_backward, OUTPUT);
+pinMode(rightMotor_forward,OUTPUT);
+pinMode(rightMotor_backward, OUTPUT);
+pinMode(trigPin, OUTPUT);
+pinMode(echoPin, INPUT);
+Serial.begin(9600);
+}
+
+void backward(int duration){  
+digitalWrite(leftMotor_forward, LOW);
+digitalWrite(leftMotor_backward, HIGH);
+digitalWrite(rightMotor_forward, LOW);
+digitalWrite(rightMotor_backward, HIGH);
+delay(duration);
+}
+
+void pivotRight(int pivotDuration){
+  digitalWrite(leftMotor_forward,HIGH);
+  digitalWrite(leftMotor_backward,LOW);
+  digitalWrite(rightMotor_forward, LOW);
+  digitalWrite(rightMotor_backward, HIGH);
+  delay(pivotDuration);
+}
+
+void loop() {
+  backward(1600);
+
+  sensorValue = analogRead(0); // read analog input pin 0
+  Serial.print("AirQuality=");
+  Serial.print(sensorValue, DEC); // prints the value read
+  Serial.println(" PPM\t");
+
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(1000);
+   digitalWrite(trigPin, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration*0.034/2;
+  Serial.print("\t\t\t\t\t\t\t\tDistance:");
+  Serial.println(distance);
+
+  if (distance <= 10 ){
+    pivotRight(7000);
+  }
+}
